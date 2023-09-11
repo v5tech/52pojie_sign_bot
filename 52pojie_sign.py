9489b1efba961e98972023f5cda37a5de77ce9dd
冯靖 <sxyx2008@163.com>
2023-09-11 14:09:30 +0800

    Update 52pojie_sign.py

--- 52pojie_sign.py
+++ 52pojie_sign.py
@@ -1,78 +1,67 @@
 # -- coding: utf-8 --
 
-import os
-import sys
+import re
+from datetime import datetime, timedelta
 import urllib.parse
 import requests
 from bs4 import BeautifulSoup
+import notify
 
+SESSION = requests.Session()
 
-def get_env_cookie():
-    cookie = ""
-    if cookie == "":
-        if os.environ.get("52POJIE_COOKIE"):
-            cookie = os.environ.get("52POJIE_COOKIE")
-        else:
-            print("请配置环境变量52POJIE_COOKIE")
-            sys.exit()
-    return cookie
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+}
 
-
-def get_input_cookie():
-    cookie = ""
-    if not cookie:
-        cookie = input("请输入52pojie cookie的值：\n")
-    return cookie
+BASE_URL = "https://www.52pojie.cn"
 
 
 def sign():
-    cookie = get_input_cookie()
-    url1 = "https://www.52pojie.cn/CSPDREL2hvbWUucGhwP21vZD10YXNrJmRvPWRyYXcmaWQ9Mg==?wzwscspd=MC4wLjAuMA=="
-    url2 = 'https://www.52pojie.cn/home.php?mod=task&do=apply&id=2&referer=%2F'
-    url3 = 'https://www.52pojie.cn/home.php?mod=task&do=draw&id=2'
-    cookie = urllib.parse.unquote(cookie)
-    cookie_list = cookie.split(";")
-    cookie = ''
-    for i in cookie_list:
-        key = i.split("=")[0]
-        if "htVC_2132_saltkey" in key:
-            cookie += "htVC_2132_saltkey=" + urllib.parse.quote(i.split("=")[1]) + "; "
-        if "htVC_2132_auth" in key:
-            cookie += "htVC_2132_auth=" + urllib.parse.quote(i.split("=")[1]) + ";"
-    if not ('htVC_2132_saltkey' in cookie or 'htVC_2132_auth' in cookie):
-        print("cookie中未包含htVC_2132_saltkey或htVC_2132_auth字段，请检查cookie")
-        sys.exit()
-    headers = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,"
-                  "application/signed-exchange;v=b3;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "zh-CN,zh;q=0.9",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive",
-        "Cookie": cookie,
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/109.0.0.0 Safari/537.36",
-    }
-    r = requests.get(url1, headers=headers, allow_redirects=False)
-    s_cookie = r.headers['Set-Cookie']
-    cookie = cookie + s_cookie
-    headers['Cookie'] = cookie
-    r = requests.get(url2, headers=headers, allow_redirects=False)
-    s_cookie = r.headers['Set-Cookie']
-    cookie = cookie + s_cookie
-    headers['Cookie'] = cookie
-    r = requests.get(url3, headers=headers)
-    r_data = BeautifulSoup(r.text, "html.parser")
-    jx_data = r_data.find("div", id="messagetext").find("p").text
-    if "您需要先登录才能继续本操作" in jx_data:
-        print("Cookie 失效")
-    elif "恭喜" in jx_data:
-        print("签到成功")
-    elif "不是进行中的任务" in jx_data:
-        print("今日已签到")
-    else:
-        print("签到失败")
+    message = "签到成功"
+    # url1 = f"{BASE_URL}/CSPDREL2hvbWUucGhwP21vZD10YXNrJmRvPWRyYXcmaWQ9Mg==?wzwscspd=MC4wLjAuMA=="
+    url2 = f'{BASE_URL}/home.php?mod=task&do=apply&id=2&referer=%2F'
+    # url3 = f'{BASE_URL}/home.php?mod=task&do=draw&id=2'
+
+    # r = SESSION.get(url1, headers=HEADERS, allow_redirects=False)
+    # r.raise_for_status()  # 判断请求状态是否正常
+
+    r = SESSION.get(url2, headers=HEADERS, allow_redirects=False)
+    r.raise_for_status()  # 判断请求状态是否正常
+
+    # r = SESSION.get(url3, headers=HEADERS)
+    # r.raise_for_status()  # 判断请求状态是否正常
+    #
+    # r_data = BeautifulSoup(r.text, "html.parser")
+    # jx_data = r_data.find("div", id="messagetext").find("p").text
+    #
+    # if "您需要先登录才能继续本操作" in jx_data:
+    #     message = "Cookie 失效"
+    # elif "恭喜" in jx_data:
+    #     message = "签到成功"
+    # elif "不是进行中的任务" in jx_data:
+    #     message = "今日已签到"
+    # else:
+    #     message = "签到失败"
+
+    message = (datetime.utcnow() + timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S") + "\r\n" + message
+    print(message)
+    notify.send("52pojie签到", message)
+
+
+def parse_cookie(raw_cookie):
+    pattern = r"(\S+)(_saltkey|_auth)=(\S+);"
+    matches = re.findall(pattern, raw_cookie)
+    cookies = {}
+    for match in matches:
+        cookies[match[0] + match[1]] = urllib.parse.quote(match[2], safe='')
+    return cookies
 
 
 if __name__ == '__main__':
-    sign()
+    try:
+        input_cookie = input("请输入52pojie cookie的值：\r\n").strip()
+        cookies = parse_cookie(input_cookie)
+        SESSION.cookies.update(cookies)
+        sign()
+    except requests.exceptions.RequestException as e:
+        print(e)
